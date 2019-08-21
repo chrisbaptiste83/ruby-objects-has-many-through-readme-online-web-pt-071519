6c1e4348b829497518924fdc52721939c608999f
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-08-21 16:52:08 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -20,10 +20,12 @@ class Customer
   def meals
   Meal.all.select do |meal|
     meal.customer == self
-  end
+  end 
+end 
   
   def waiters
   meals.map do |meal|
     meal.waiter
-  end
+  end 
+end 
 end
